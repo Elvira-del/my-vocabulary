148ae71d59c956db2c380eab2031eece25de4e45
Elvira Krutova <ninelostr@gmail.com>
2024-12-24 20:03:47 +0000

    feat: add word to vocabulary list

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,8 @@
 import { create } from "zustand";
+import { v4 as uuidv4 } from "uuid";
 
 type VocabularyElem = {
+  id: string;
   word: string;
   class: string;
   definition: string;
@@ -8,13 +10,15 @@ type VocabularyElem = {
 
 type VocabularyStore = {
   vocabulary: VocabularyElem[];
-  addWord: (word: VocabularyElem) => void;
+  addWord: (word: Omit<VocabularyElem, "id">) => void;
 };
 
 const useVocabularyStore = create<VocabularyStore>((set) => ({
   vocabulary: [],
-  addWord: (word: VocabularyElem) =>
-    set((state) => ({ vocabulary: [...state.vocabulary, word] })),
+  addWord: (word) =>
+    set((state) => ({
+      vocabulary: [...state.vocabulary, { id: uuidv4(), ...word }],
+    })),
 }));
 
 export default useVocabularyStore;
