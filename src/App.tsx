9ac8abaa41872292021d2ea674ffea8d0ece71b2
Elvira Krutova <ninelostr@gmail.com>
2024-12-24 08:59:50 +0000

    feat: add modal for adding new word

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Layout } from "./components/Layout/Layout";
+import { AddWordDialog } from "./components/AddWordDialog/AddWordDialog";
 import "./App.css";
 
 function App() {
   return (
     <Layout>
       <h1>App title</h1>
+      <AddWordDialog />
     </Layout>
   );
 }
--- src/components/AddWordButton/AddWordButton.tsx
+++ src/components/AddWordButton/AddWordButton.tsx
@@ -1,9 +1,14 @@
+import { FC } from "react";
 import { Fab } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 
-export const AddWordButton = () => {
+type AddWordButtonProps = {
+  onOpen: () => void;
+};
+
+export const AddWordButton: FC<AddWordButtonProps> = ({ onOpen }) => {
   return (
-    <Fab color="primary" aria-label="Add word">
+    <Fab color="primary" onClick={onOpen} aria-label="Add word">
       <AddIcon />
     </Fab>
   );
