20dc86274c5e9fe5ebbc27b5d092d9634c41449d
Elvira Krutova <ninelostr@gmail.com>
2025-06-28 23:46:47 +0100

    test: add test for DeleteDialog component

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
@@ -50,5 +51,8 @@ export default defineConfig({
     alias: {
       src: "/src",
     },
+  },
+  test: {
+    environment: "jsdom",
   },
 });
